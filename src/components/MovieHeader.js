23212e3b2183b0d1543da69c09cbadc00e725c41
Kat <kyevsukov@gmail.com>
2021-10-05 20:05:03 -0400

    action hooked up to toggle favorite state

--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { toggleFavorites } from './../actions/favoritesActions'
 
-const MovieHeader = ({ appTitle, displayFavorites }) => {
+const MovieHeader = ({ appTitle, displayFavorites, toggleFavorites }) => {
     // const appTitle = "";
     // const displayFavorites = true;
-    
+    console.log(displayFavorites)
     return(<div className="table-title">
         <div className="row">
         <div className="col-sm-6">
             <h2>{appTitle}</h2>
         </div>
         <div className="col-sm-6 headerBar">
-            <div className="btn btn-sm btn-primary"><span>{ displayFavorites ? "Hide" : "Show"} Favorites</span></div>
+            <div onClick={() => {toggleFavorites()}} className="btn btn-sm btn-primary"><span>{ displayFavorites ? "Hide" : "Show"} Favorites</span></div>
             <Link to="/movies" className="btn btn-sm btn-primary">View All Movies</Link>
             <Link to="/movies/add" className="btn btn-sm btn-success"><i className="material-icons">&#xE147;</i> <span>Add New Movie</span></Link>
         </div>
@@ -23,7 +24,7 @@ const MovieHeader = ({ appTitle, displayFavorites }) => {
 const mapStateToProps = (state) => {
     return{
         appTitle: state.movieState.appTitle,
-        displayFavorites: state.movieState.displayFavorites
+        displayFavorites: state.favoritesState.displayFavorites
     }
 }
-export default connect(mapStateToProps, {})(MovieHeader);
+export default connect(mapStateToProps, { toggleFavorites })(MovieHeader);
--- src/reducers/favoritesReducer.js
+++ src/reducers/favoritesReducer.js
@@ -1,9 +1,18 @@
+import { TOGGLE_FAVORITES } from './../actions/favoritesActions'
+
 const initialState = {
     favorites: [],
     displayFavorites: true
 }
 const reducer = (state = initialState, action) => {
+    console.log(state)
     switch(action.type){
+        case TOGGLE_FAVORITES:
+            return{
+                ...state,
+                displayFavorites: !state.displayFavorites
+                
+            }
         default:
             return state;
     }
